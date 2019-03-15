dfb16f6b6246e792a5f9b32a038a020079abe8a7
k00ziex <andreas_vorgaard@hotmail.com>
2019-03-15 13:42:32 +0100

    The ever impressive, the long contained, often imitated, but never duplicated … Andy of the Back-end Boys!

--- Database/Database/Entities/Barrepræsentant.cs
+++ Database/Database/Entities/Barrepræsentant.cs
@@ -6,10 +6,15 @@ namespace Database
     {
         [MaxLength(50)]
         public string BrugerNavn { get; set; }
+
+        [Required]
         [MaxLength(150)]
         public string Navn { get; set; }
+
         [MaxLength(150)]
         public string BarNavn { get; set; }
+
+        [Required]
         [MaxLength(50)]
         public string Password { get; set; } //Skal krypteres!
 
