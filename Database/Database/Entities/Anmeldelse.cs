0466c9a88d496ec7104a0bacc1c138fa693e8a9a
Simon Jensen <simonzhjensen@gmail.com>
2019-03-15 13:42:48 +0100

    Even the smallest person can change the course of the future

--- Database/Database/Entities/Anmeldelse.cs
+++ Database/Database/Entities/Anmeldelse.cs
@@ -10,9 +10,12 @@ namespace Database
         [Required]
         public int BarTryk { get; set; }
 
+        [MaxLength(150)]
         public string BarNavn { get; set; }
 
+        [MaxLength(50)]
         public string BrugerNavn { get; set; }
+
         public Bar Bar { get; set; }
 
         public Kunde Kunde { get; set; }
