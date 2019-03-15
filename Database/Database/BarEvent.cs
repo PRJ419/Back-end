09ab0add6daf2cba62e7e11fa50696ae858acdde
Simon Jensen <simonzhjensen@gmail.com>
2019-03-15 11:23:28 +0100

    Your clones are very impressive, you must be very proud

--- Database/Database/BarEvent.cs
+++ Database/Database/BarEvent.cs
@@ -1,7 +1,16 @@
-﻿namespace Database
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Database
 {
     public class BarEvent
     {
+        [MaxLength(150)]
+        public string BarNavn { get; set; }
         
+        [MaxLength(75)]
+        public string EventNavn { get; set; }
+
+        public DateTime Dato { get; set; }
     }
 }
--- Database/Database/Barrepræsentant.cs
+++ Database/Database/Barrepræsentant.cs
@@ -1,7 +1,17 @@
-﻿namespace Database
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database
 {
     public class Barrepræsentant
     {
-        
+        [MaxLength(50)]
+        public string BrugerNavn { get; set; }
+        [MaxLength(150)]
+        public string Navn { get; set; }
+        [MaxLength(150)]
+        public string BarNavn { get; set; }
+        [MaxLength(50)]
+        public string Password { get; set; } //Skal krypteres!
+
     }
 }
