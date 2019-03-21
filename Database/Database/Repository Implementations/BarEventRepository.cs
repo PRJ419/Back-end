6658400b11410bd33a207a10f334e1d7229e3ee5
Simon Jensen <simonzhjensen@gmail.com>
2019-03-21 13:58:28 +0100

    BarEvent and Customer repo, Updated

---
+++ Database/Database/Repository Implementations/BarEventRepository.cs
@@ -0,0 +1,38 @@
+﻿using Database.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Repository_Implementations
+{
+    public class BarEventRepository : IBarEventRepository
+    {
+        private DbContext _dbContext;
+
+        public BarEventRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public BarEvent Get(string barEvent)
+        {
+            return _dbContext.Set<BarEvent>().Find(barEvent);
+        }
+
+        public void Add(BarEvent barEvent)
+        {
+            _dbContext.Set<BarEvent>().Add(barEvent);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(string barEvent)
+        {
+            _dbContext.Set<BarEvent>().Remove(Get(barEvent));
+            _dbContext.SaveChanges();
+        }
+
+        public void Edit(BarEvent barEvent)
+        {
+            _dbContext.Entry(barEvent).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Database/Database/Repository Implementations/CustomerRepository.cs
@@ -0,0 +1,38 @@
+﻿using Database.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Repository_Implementations
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private DbContext _dbContext;
+
+        public CustomerRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Customer Get(string customer)
+        {
+            return _dbContext.Set<Customer>().Find(customer);
+        }
+
+        public void Add(Customer customer)
+        {
+            _dbContext.Set<Customer>().Add(customer);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(string customer)
+        {
+            _dbContext.Set<Customer>().Remove(Get(customer));
+            _dbContext.SaveChanges();
+        }
+
+        public void Edit(Customer customer)
+        {
+            _dbContext.Entry(customer).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+    }
+}
