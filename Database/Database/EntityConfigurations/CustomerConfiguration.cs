a892facc96ab0b0fce237ec4104b6a8d9087f806
Simon Jensen <simonzhjensen@gmail.com>
2019-03-22 11:40:34 +0100

    Code Genocide

--- Database/Database/EntityConfigurations/CustomerConfiguration.cs
+++ Database/Database/EntityConfigurations/CustomerConfiguration.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Database.EntityConfigurations
 {
-    class CustomerConfiguration
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
     {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder
+                .HasKey(a => a.Username);
+
+            builder
+                .HasIndex(a => a.Email)
+                .IsUnique();
+        }
     }
 }
