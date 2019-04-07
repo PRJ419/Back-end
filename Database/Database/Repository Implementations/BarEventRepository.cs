99470808e89577c177a4a9a3495d8e6feaff58d4
k00ziex <andreas_vorgaard@hotmail.com>
2019-04-07 11:45:16 +0200

    Lots of documentation!

--- Database/Database/Repository Implementations/BarEventRepository.cs
+++ Database/Database/Repository Implementations/BarEventRepository.cs
@@ -4,12 +4,25 @@ namespace Database.Repository_Implementations
 {
     public class BarEventRepository : Repository<BarEvent>
     {
+        /// <summary>
+        /// Takes the database context and sends it to the base class constructor (Repository).
+        /// </summary>
+        /// <param name="dbContext">
+        /// Takes a database context that's gonna be set, so you can access the db.
+        /// </param>
         public BarEventRepository(BarOMeterContext dbContext) : base(dbContext)
         {
 
         }
 
-
+        /// <summary>
+        /// This method is for editing a BarEvent entity that already exists in the db.
+        /// Therefore the keys of the edited entity has to correspond to the one in the db.
+        /// </summary>
+        /// <param name="entity">
+        /// Takes an edited BarEvent entity as parameter and finds the entity in the db,
+        /// with the same keys.
+        /// </param>
         public void Edit(BarEvent entity)
         {
             var OldEvent = Get(entity.BarName, entity.EventName);
--- Database/Database/Repository Implementations/CustomerRepository.cs
+++ Database/Database/Repository Implementations/CustomerRepository.cs
@@ -9,10 +9,25 @@ namespace Database.Repository_Implementations
 {
     public class CustomerRepository : Repository<Customer>
     {
+        /// <summary>
+        /// Takes the database context and sends it to the base class constructor (Repository).
+        /// </summary>
+        /// <param name="dbContext">
+        /// Takes a database context that's gonna be set, so you can access the db.
+        /// </param>
         public CustomerRepository(BarOMeterContext dbContext) : base(dbContext)
         {
         }
 
+
+        /// <summary>
+        /// This function is for editing a customer entity in the database. If the keys of the
+        /// parameter entity doesn't exist in the database, this function fails.
+        /// </summary>
+        /// <param name="entity">
+        /// Takes an edited customer as a parameter. It is therefore a precondition,
+        /// that the keys of the customer hasn't been changed.
+        /// </param>
         public void Edit(Customer entity)
         {
             var oldCustomer = Get(entity.Username);
