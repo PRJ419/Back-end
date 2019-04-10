f12208f239d0187af1796d56d1b9437243a46f9e
Simon Jensen <simonzhjensen@gmail.com>
2019-04-10 15:09:09 +0200

    Doxygen kommentarer tilføjet til Coupon, Customer, Drink og Review

--- Database/Database/Entities/Coupon.cs
+++ Database/Database/Entities/Coupon.cs
@@ -6,14 +6,26 @@ namespace Database
 {
     public class Coupon
     {
+        /// <summary>
+        /// Property for getting and setting the bar name associated to the coupon
+        /// </summary>
         [MaxLength(150)]
         public string BarName { get; set; }
 
+        /// <summary>
+        /// Property for getting and setting the coupon id for a coupon
+        /// </summary>
         [MaxLength(50)]
         public string CouponID { get; set; }
 
+        /// <summary>
+        /// Property for getting and setting the expiration date of a coupon
+        /// </summary>
         public DateTime ExpirationDate { get; set; }
 
+        /// <summary>
+        /// Navigational property required by the database for finding the bar associated to the coupon
+        /// </summary>
         public virtual Bar Bar { get; set; }
     }
 }
--- Database/Database/Entities/Customer.cs
+++ Database/Database/Entities/Customer.cs
@@ -8,27 +8,47 @@ namespace Database
 {
     public class Customer
     {
-        
+        /// <summary>
+        /// Property for getting and setting the username of a Customer
+        /// </summary>
         [MaxLength(50)]
         public string Username { get; set; }
 
+        /// <summary>
+        /// Property for getting and setting the name of a customer
+        /// </summary>
         [Required]
         [MaxLength(150)]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Property for getting and setting the birthday of a customer
+        /// </summary>
         [Required]
         public DateTime DateOfBirth { get; set; }
 
+        /// <summary>
+        /// Property for getting and setting the Email of a customer
+        /// </summary>
         [Required]
         [MaxLength(150)]
         public string Email { get; set; }
 
+        /// <summary>
+        /// Property for getting and setting the favorite bar of a customer
+        /// </summary>
         [MaxLength(150)]
         public string FavoriteBar { get; set; }
 
+        /// <summary>
+        /// Property for getting and setting the favorite drink of a customer
+        /// </summary>
         [MaxLength(50)]
         public string FavoriteDrink { get; set; }
 
+        /// <summary>
+        /// Navigational property required by the database for finding the reviews associated to the customer
+        /// </summary>
         public virtual List<Review> Reviews { get; set; }
     }
 }
