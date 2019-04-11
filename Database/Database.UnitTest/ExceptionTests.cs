08cbc3ea8258f8d334399fdd584395be06757229
k00ziex <andreas_vorgaard@hotmail.com>
2019-04-11 11:57:46 +0200

    Preparation of the unit testing of the DB

---
+++ Database/Database.UnitTest/ExceptionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Database.UnitTest
+{
+    public class ExceptionTests
+    {
+        [SetUp]
+        public void setup()
+        {
+            // HERE'S THE SETUP
+        }
+
+        [Test]
+        public void TestFunction()
+        {
+            // HERE'S THE TEST FUNCTION
+        }
+    }
+}
