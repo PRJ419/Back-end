4788842426f2e6c7d20135fc5441388533b123c1
Simon Jensen <simonzhjensen@gmail.com>
2019-04-30 11:28:32 +0200

    Missed a commit

--- Database/Database/Repository Implementations/BarrepresentativeRepository.cs
+++ Database/Database/Repository Implementations/BarrepresentativeRepository.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Database.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Database.Repository_Implementations
 {
-    public class BarRepresentativeRepository : Repository<BarRepresentative>
+    public class BarRepresentativeRepository : Repository<BarRepresentative>,IBarRepresentativeRepository
     {
         /// <summary>
         /// Takes the database context and sends it to the base class constructor (Repository).
@@ -20,15 +21,7 @@ namespace Database.Repository_Implementations
         }
 
 
-        /// <summary>
-        /// This method is for editing a BarRepresentative entity that's already in the database.
-        /// Therefore the key of the edited entity (parameter) has to correspond
-        /// to the key of an entity in the database.
-        /// </summary>
-        /// <param name="entity">
-        /// Takes an edited BarRepresentative entity and edits the corresponding entity
-        /// in the database.
-        /// </param>
+       
         public void Edit(BarRepresentative entity)
         {
             var oldBarRep = Get(entity.Username);
