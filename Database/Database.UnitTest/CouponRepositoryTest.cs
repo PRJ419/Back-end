69e9d15544bcac3fc87c23c0777640ef6110f3be
Simon Jensen <simonzhjensen@gmail.com>
2019-05-01 14:44:53 +0200

    Coupon Tests Fixed

--- Database/Database.UnitTest/CouponRepositoryTest.cs
+++ Database/Database.UnitTest/CouponRepositoryTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Database.Repository_Implementations;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 
@@ -12,40 +13,128 @@ namespace Database.UnitTest
     [TestFixture]
     class CouponRepositoryTest
     {
-        private CouponRepository _repository;
+        private CouponRepository _uut;
         private DbContextOptions<BarOMeterContext> _options;
         private BarOMeterContext _context;
+        private SqliteConnection _connection;
+
+        [SetUp]
+        public void Setup()
+        {
+            _connection = new SqliteConnection("Datasource=:memory:");
+            _connection.Open();
+            _options =
+                new DbContextOptionsBuilder<BarOMeterContext>().UseSqlite(_connection).Options;
+            _context = new BarOMeterContext(_options);
+            _uut = new CouponRepository(_context);
+            _context.Database.EnsureCreated();
+        }
 
         [Test]
         public void CouponRepository_EditExistingCoupon_CouponIsEdited()
         {
-            _options =
-                new DbContextOptionsBuilder<BarOMeterContext>().UseInMemoryDatabase(databaseName: "EditCoupon")
-                    .Options;
-            _context = new BarOMeterContext(_options);
-            _repository = new CouponRepository(_context);
-
+     
             var coupon = new Coupon()
             {
-                BarName = "Testbar",
+                BarName = "Katrines Kælder",
                 CouponID = "Coupon123Test",
                 ExpirationDate = new DateTime(1997,01,01),
             };
-            _repository.Add(coupon);
+            _uut.Add(coupon);
             _context.SaveChanges();
 
             var editedCoupon = new Coupon()
             {
-                BarName = "Testbar",
+                BarName = "Katrines Kælder",
                 CouponID = "Coupon123Test",
                 ExpirationDate = new DateTime(2019,12,12),
             };
-            _repository.Edit(editedCoupon);
+            _uut.Edit(editedCoupon);
             _context.SaveChanges();
 
             Assert.AreEqual(new DateTime(2019,12,12), 
-                    _repository.Get("Coupon123Test", "Testbar").ExpirationDate);
+                    _uut.Get("Coupon123Test", "Katrines Kælder").ExpirationDate);
 
         }
+
+
+        [Test]
+        public void Edit_Entity_WithoutChangingAnyInformation()
+        {
+            var coupon = new Coupon()
+            {
+                BarName = "Katrines Kælder",
+                CouponID = "Coupon123Test",
+                ExpirationDate = new DateTime(1997, 01, 01),
+            };
+            _uut.Add(coupon);
+            _context.SaveChanges();
+
+            var editedCoupon = new Coupon()
+            {
+                BarName = "Katrines Kælder",
+                CouponID = "Coupon123Test",
+                ExpirationDate = new DateTime(1997, 01, 01),
+            };
+            _uut.Edit(editedCoupon);
+            _context.SaveChanges();
+
+            Assert.AreEqual(new DateTime(1997, 01, 01),
+                _uut.Get("Coupon123Test", "Katrines Kælder").ExpirationDate);
+        }
+
+        [Test]
+        public void Add_Multiple_Coupons()
+        {
+            var coupon = new Coupon()
+            {
+                BarName = "Katrines Kælder",
+                CouponID = "Coupon123Test",
+                ExpirationDate = new DateTime(1997, 01, 01),
+            };
+            _uut.Add(coupon);
+            
+
+            var coupon2 = new Coupon()
+            {
+                BarName = "Katrines Kælder",
+                CouponID = "Coupon123Test2",
+                ExpirationDate = new DateTime(2010, 01, 01),
+            };
+            _uut.Add(coupon2);
+
+            Assert.AreEqual(2,_context.SaveChanges());
+        }
+
+        [Test]
+        public void Constraint_On_CouponID_And_BarName()
+        {
+            var coupon = new Coupon()
+            {
+                BarName = "Katrines Kælder",
+                CouponID = "Coupon123Test",
+                ExpirationDate = new DateTime(1997, 01, 01),
+            };
+            _uut.Add(coupon);
+
+
+            var coupon2 = new Coupon()
+            {
+                BarName = "Katrines Kælder",
+                CouponID = "Coupon123Test",
+                ExpirationDate = new DateTime(2010, 01, 01),
+            };
+            ;
+
+            Assert.That(() => _uut.Add(coupon2), Throws.Exception);
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _connection.Close();
+        }
+
     }
 }
