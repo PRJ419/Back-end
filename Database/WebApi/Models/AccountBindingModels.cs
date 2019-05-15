e83bc9cbb29e6acb65c7b4f2891dd0e7efac172a
Laurits Dyrberg Hansen <201706994@post.au.dk>
2019-05-15 13:25:03 +0200

    Removed requirements for password

--- Database/WebApi/Models/AccountBindingModels.cs
+++ Database/WebApi/Models/AccountBindingModels.cs
@@ -16,7 +16,7 @@ namespace WebApi.Models
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 0)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
@@ -57,7 +57,7 @@ namespace WebApi.Models
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 0)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
@@ -102,7 +102,7 @@ namespace WebApi.Models
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 0)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
--- Database/WebApi/Startup.cs
+++ Database/WebApi/Startup.cs
@@ -66,12 +66,12 @@ namespace WebApi
             services.Configure<IdentityOptions>(options =>
             {
                 // Password settings.
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
                 options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = true;
-                options.Password.RequiredLength = 6;
-                options.Password.RequiredUniqueChars = 1;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 0;
+                options.Password.RequiredUniqueChars = 0;
 
                 // User settings.
                 options.User.AllowedUserNameCharacters =
